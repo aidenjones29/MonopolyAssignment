f1ce572c0daf6c2b48eac1b2d67aa0fb3f59996b
aidenjones29 <36414445+aidenjones29@users.noreply.github.com>
2019-03-11 15:54:26 +0000

    classes
    adw

---
+++ MonopolyAss/Classes.cpp
@@ -0,0 +1,48 @@
+#include "Classes.h"
+
+class CBase
+{
+public:
+	virtual ~CBase(){};
+	virtual 
+};
+
+class CProperty : CBase
+{
+
+};
+
+class CGo : CBase
+{
+
+};
+
+class CBonus : CBase
+{
+
+};
+
+class CPenalty : CBase
+{
+
+};
+
+class CFreeParking : CBase
+{
+
+};
+
+class CJail : CBase
+{
+
+};
+
+class CGoToJail : CBase
+{
+
+};
+
+class CAirport : CBase
+{
+
+};
--- MonopolyAss/Main.cpp
+++ MonopolyAss/Main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <fstream>
 #include "RandomNum.h"
+#include "Classes.h"
+
+
 
 int main()
 {
+
 	int seed = 5;
 	int randomNumber[10];
 
 	Random(randomNumber, seed);
 	system("Pause");
+}
+
+void loadFile()
+{
+
 }
